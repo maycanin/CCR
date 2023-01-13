e5fd2af4225ae87d6cf31e5ece39a508084292f9
maycanin <maysoninacio@gmail.com>
2023-01-13 08:12:52 -0300

    implementando area de login

--- js/login.js
+++ js/login.js
@@ -2,19 +2,35 @@ const cadastro = document.getElementById('cadastro')
 const relatorio = document.getElementById('relatorio')
 
 function login() {
-    const login = document.getElementById("nomeUsuario").value;
-    const senha = document.getElementById("senhaUsuario").value;
-    if (login == "admin" && senha == "admin") {
-        relatorio.style.display = 'block';
-        cadastro.style.display = 'block';
-    } else {
-        loginNegado()
-    }
+  const login = document.getElementById('nomeUsuario').value
+  const senha = document.getElementById('senhaUsuario').value
+    if (login == 'admin' && senha == 'admin') {
+      localStorage.setItem('estaLogado', true)
+    window.location.replace('/cadastroConsulta.html')
+  } else {
+    loginNegado()
+  }
 }
 
 function loginNegado() {
-    const erroDiv = document.getElementById("erro")
-    erroDiv.innerText = "Informações inválidas!"
-    erroDiv.style.color = "red"
-    erroDiv.style.textAlign = "center"
+  const toast = document.getElementById('toast')
+  const toastTitle = document.getElementById('toastTitle')
+  const toastDescription = document.getElementById('toastDescription')
+  toastTitle.innerText = 'Informações inválidas!'
+  toastDescription.innerText = 'Usuário e/ou senha incorreto(s)'
+  toast.classList.add('error', 'show')
+  setTimeout(() => {
+    toast.classList.remove('show')
+  }, 5000)
+}
+
+function verificaLogin() {
+    if (localStorage.getItem('estaLogado') !== "true") {
+        window.location.replace('/index.html')
+    }
+}
+
+function deslogar() {
+    localStorage.removeItem('estaLogado')
+    window.location.replace('/index.html')
 }
