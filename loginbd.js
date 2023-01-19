6d7f8beb38ce46d947d341143a6ca83123efcae6
maycanin <maysoninacio@gmail.com>
2023-01-18 21:55:24 -0300

    Progresso no CSS e na Responsividade. #26

--- loginbd.js
+++
@@ -1,7 +0,0 @@
-var bd = openDatabase("meuBD", "1.0", "Meu Banco de Dados", 4080);
-
-bd.transaction(function (criar) {
-    criar.executeSql(
-        "CREATE TABLE login (usuario TEXT, senha TEXT)"
-    );
-});
