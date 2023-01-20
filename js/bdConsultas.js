a8b6c3b0e89cb0a6bd0c5bbde155526146051e7c
alexsandro_cm <alexsandrotecn@gmail.com>
2023-01-20 09:11:18 -0300

    processo de trabalho

--- js/bdConsultas.js
+++ js/bdConsultas.js
@@ -108,3 +108,55 @@ function maskCurrency(valor, locale = "pt-BR", currency = "BRL") {
         currency,
     }).format(valor);
 }
+
+function adcionaConveniosPadroes() {
+    criaBDConvenios();
+
+    const convenios = ["Unimed", "Santa Rita", "Solumed", "Prever"];
+    const conveniosCadastros = coletaJaInseridos();
+    console.log(conveniosCadastros);
+
+    const conveniosParaAdd = listaConveniosParaAdd(
+        convenios,
+        conveniosCadastros
+    );
+    console.log(conveniosParaAdd);
+}
+
+function criaBDConvenios() {
+    bd.transaction(function (criar) {
+        criar.executeSql("CREATE TABLE convenios (nome TEXT)");
+    });
+}
+
+function coletaJaInseridos() {
+    let a = [];
+    bd.transaction(function (ler) {
+        ler.executeSql(
+            "SELECT * FROM convenios",
+            [],
+            function (ler, resultados) {
+                const conveniosCadastros = new Array(resultados.rows.length);
+                console.log(resultados.rows.length);
+                for (let i = 0; i < resultados.rows.length; i++) {
+                    console.log(resultados.rows.item(i).nome);
+                    conveniosCadastros[i] = resultados.rows.item(i).nome;
+                }
+                a = conveniosCadastros;
+            }
+        );
+    });
+    return a;
+}
+
+function listaConveniosParaAdd(convenios, conveniosCadastros) {
+    const conveniosParaAdd = [];
+    convenios.forEach((e) =>
+        conveniosCadastros.includes(e)
+            ? conveniosParaAdd
+            : conveniosParaAdd.push(e)
+    );
+    return conveniosParaAdd;
+}
+
+adcionaConveniosPadroes();
--- js/dados.js
+++ js/dados.js
@@ -2,65 +2,65 @@ const lista = [
     {
         id: "001",
         nomeDoPaciente: "Maria do Carmo",
-        data: "2022-10-28T15:09:00",
+        data: "2022-10-28T15:09",
         convenio: "Unimed",
-        valorConsulta: "100,00",
+        valorConsulta: 100.0,
         imgGuia: "https://picsum.photos/200/300",
     },
     {
         id: "002",
         nomeDoPaciente: "Mario do Parmo",
-        data: "2022-01-28T15:09:00",
+        data: "2022-01-28T15:09",
         convenio: "Santa Rita",
-        valorConsulta: "150,00",
+        valorConsulta: 150.0,
         imgGuia: null,
     },
     {
         id: "003",
         nomeDoPaciente: "Mariana do Carmo",
-        data: "2022-03-28T15:09:00",
+        data: "2022-03-28T15:09",
         convenio: "Solumed",
-        valorConsulta: "230,00",
+        valorConsulta: 230.0,
         imgGuia: "https://picsum.photos/200/300",
     },
     {
         id: "004",
         nomeDoPaciente: "Marrie de Carme",
-        data: "2022-09-28T15:09:00",
+        data: "2022-09-28T15:09",
         convenio: "Prever",
-        valorConsulta: "200,00",
+        valorConsulta: 200.0,
         imgGuia: "https://picsum.photos/200/300",
     },
     {
         id: "005",
         nomeDoPaciente: "Marcia do Carmo",
-        data: "2022-10-28T15:09:00",
+        data: "2022-10-28T15:09",
         convenio: "Unimed",
-        valorConsulta: "101,00",
+        valorConsulta: 101.0,
         imgGuia: "https://picsum.photos/200/300",
     },
     {
         id: "006",
         nomeDoPaciente: "Marcio do Parmo",
-        data: "2022-01-28T15:09:00",
+        data: "2022-01-28T15:09",
         convenio: "Santa Rita",
-        valorConsulta: "10,00",
+        valorConsulta: 10.0,
         imgGuia: "https://picsum.photos/200/300",
     },
     {
         id: "007",
         nomeDoPaciente: "Mariana dos Santos",
-        data: "2022-03-28T15:09:00",
+        data: "2022-03-28T15:09",
         convenio: "Solumed",
-        valorConsulta: "20,00",
+        valorConsulta: 20.0,
         imgGuia: "https://picsum.photos/200/300",
     },
     {
         id: "008",
         nomeDoPaciente: "Marrie de Carmolina",
-        data: "2022-09-28T15:09:00",
+        data: "2022-09-28T15:09",
         convenio: "Prever",
-        valorConsulta: "220,00",
+        valorConsulta: 220.0,
         imgGuia: null,
     },
 ];
@@ -68,6 +68,7 @@ const lista = [
 lista.forEach((e) => insereBD(e));
 
 function insereBD(elemento) {
+    console.log(elemento);
     if (confereDuplicata(elemento)) {
     } else {
         //insere
@@ -92,11 +93,11 @@ function confereDuplicata(elemento) {
     //confere se não esta
     bd.transaction(function (ler) {
         ler.executeSql(
-            "SELECT * FROM convenio",
+            `SELECT * FROM convenio WHERE nome=${elemento.nomeUsuario} AND convenio=${elemento.convenio} AND valor=${elemento.valorDaConsulta} `,
             [],
             function (ler, resultados) {
-                //
-                // return true;
+                console.log(resultados);
+                return true;
             }
         );
     });
